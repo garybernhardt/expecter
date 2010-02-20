3b4c11e5af4da33741f5d48a9f994f0c5e273346
Gary Bernhardt <gary.bernhardt@gmail.com>
2010-02-19 21:52:15 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='expectergadget',
+      version='0.0',
+      description='Better expectation library',
+      author='Gary Bernhardt',
+      author_email='gary.bernhardt@gmail.com',
+      py_modules=['expecter'],
+      license='BSD',
+     )
