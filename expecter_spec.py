e28caf7e3751191e598cd826baf320df846d1bef
Gary Bernhardt <gary.bernhardt@gmail.com>
2010-02-19 21:51:00 -0500

    Removed references to old "raises" function from mote

--- expecter_spec.py
+++ expecter_spec.py
@@ -1,5 +1,6 @@
 from __future__ import with_statement
 import doctest
+from nose.tools import assert_raises
 
 from expecter import expect
 
@@ -15,25 +16,25 @@ def describe_expecter():
     def expects_equals():
         expect(2) == 1 + 1
         def _fails(): expect(1) == 2
-        assert raises(AssertionError, _fails)
+        assert_raises(AssertionError, _fails)
         assert fail_msg(_fails) == 'Expected 1 but got 2'
 
     def expects_not_equals():
         expect(1) != 2
         def _fails(): expect(1) != 1
-        assert raises(AssertionError, _fails)
+        assert_raises(AssertionError, _fails)
         assert fail_msg(_fails) == 'Expected anything except 1 but got it'
 
     def expects_less_than():
         expect(1) < 2
         def _fails(): expect(1) < 0
-        assert raises(AssertionError, _fails)
+        assert_raises(AssertionError, _fails)
         assert fail_msg(_fails) == 'Expected something less than 0 but got 1'
 
     def expects_greater_than():
         expect(2) > 1
         def _fails(): expect(0) > 1
-        assert raises(AssertionError, _fails)
+        assert_raises(AssertionError, _fails)
         assert fail_msg(_fails) == (
             'Expected something greater than 1 but got 0')
 
@@ -41,7 +42,7 @@ def describe_expecter():
         expect(1) <= 1
         expect(1) <= 2
         def _fails(): expect(2) <= 1
-        assert raises(AssertionError, _fails)
+        assert_raises(AssertionError, _fails)
         assert fail_msg(_fails) == (
             'Expected something less than or equal to 1 but got 2')
 
@@ -49,7 +50,7 @@ def describe_expecter():
         expect(1) >= 1
         expect(2) >= 1
         def _fails(): expect(1) >= 2
-        assert raises(AssertionError, _fails)
+        assert_raises(AssertionError, _fails)
         assert fail_msg(_fails) == (
             'Expected something greater than or equal to 2 but got 1')
 
@@ -62,7 +63,7 @@ def describe_expecter():
             def _expects_raise_but_doesnt_get_it():
                 with expect.raises(KeyError):
                     pass
-            assert raises(AssertionError, _expects_raise_but_doesnt_get_it)
+            assert_raises(AssertionError, _expects_raise_but_doesnt_get_it)
             assert fail_msg(_expects_raise_but_doesnt_get_it) == (
                 'Expected an exception of type KeyError but got none')
 
@@ -70,7 +71,7 @@ def describe_expecter():
             def _expects_key_error_but_gets_value_error():
                 with expect.raises(KeyError):
                     raise ValueError
-            assert raises(ValueError, _expects_key_error_but_gets_value_error)
+            assert_raises(ValueError, _expects_key_error_but_gets_value_error)
 
 def describe_readme():
     def passes_as_a_doctest():
