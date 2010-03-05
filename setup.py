428d0bf23725ef21d13bc34aec9125ccecb2bae5
Gary Bernhardt <gary.bernhardt@gmail.com>
2010-03-04 17:46:23 -0800

    Updated version to 0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='expectergadget',
-      version='0.0',
+      version='0.1',
       description='Better expectation library',
       author='Gary Bernhardt',
       author_email='gary.bernhardt@gmail.com',
