3bd8527174d90b82eefe231d00e7a0875aadf0b8
Gary Bernhardt <gary.bernhardt@gmail.com>
2010-03-07 09:35:39 -0800

    Added long description and URL to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='expecter',
       version='0.1.1',
       description='Better expectation library',
+      long_description=file('README').read(),
       author='Gary Bernhardt',
       author_email='gary.bernhardt@gmail.com',
       py_modules=['expecter'],
+      url='http://bitbucket.org/garybernhardt/expecter-gadget',
       license='BSD',
      )
+
