58aa2a30b3f76e51a7b6ab0b6bcafac55cace733
Gary Bernhardt <gary.bernhardt@gmail.com>
2010-03-15 12:57:40 -0700

    Updated version to 0.1.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='expecter',
-      version='0.1.2',
+      version='0.1.3',
       description='Expecter Gadget, a better expectation (assertion) library',
       long_description=file('README').read(),
       author='Gary Bernhardt',
