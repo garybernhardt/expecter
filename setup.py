374fd53e74335db91751aabc721f6dcf6ae758e7
Brian Beck <exogen@gmail.com>
2010-12-15 20:47:19 -0800

    Fixed broken reference to README - should be README.markdown.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='expecter',
       version='0.2',
       description='Expecter Gadget, a better expectation (assertion) library',
-      long_description=file('README').read(),
+      long_description=file('README.markdown').read(),
       author='Gary Bernhardt',
       author_email='gary.bernhardt@gmail.com',
       py_modules=['expecter'],
