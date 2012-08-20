8c978e2b8d6f62b309a039b82c922e7ef289fc7c
Gary Bernhardt <gary.bernhardt@gmail.com>
2012-08-20 15:53:55 -0700

    add test for docs

---
+++ tests/test_docs.py
@@ -0,0 +1,10 @@
+from subprocess import check_call
+from os.path import join, dirname
+
+
+class describe_docs:
+    def they_build_cleanly(self):
+        docs_dir = join(dirname(__file__), "..", "docs")
+        command = "(cd %s && make html) > /dev/null" % docs_dir
+        check_call(command, shell=True)
+
